661f2eef8636f6c4649b79b293fdf96897fa2bf5
Beagle User <debian@BeagleBone.localdomain>
2022-11-27 23:51:00 +0000

    added Flynn

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    bool pretraining = false;
+    bool pretraining = true;
     Face_initialize(pretraining);
     int a = Face_predict("testme1.jpg");
     int b = Face_predict("testme4.jpg");
-    int c = Face_predict("testme5.jpg");
+    int c = Face_predict("testme6.jpg");
     printf("trial 1: %d trial 2: %d trial 3: %d\n",a,b,c);
     return 0;
 }
